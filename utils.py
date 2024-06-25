fa0643f1f85acb83bcef6775a662a6e045b479fd
jacevys <jacevys@gmail.com>
2024-06-25 17:41:12 +0800

    Added functionality to retrieve MistTrack labels

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import json
 import os
 import numpy as np
+import requests
 #
 def readJson(path: str):
     with open(path, 'r', encoding='utf-8-sig') as json_file:
@@ -14,49 +15,17 @@ def saveJson(save_path, wallet_list):
 
     print('saved')
 #
-def func_1(split_ratio, version):
-    data_lists = ['others', 'exchange', 'kyc', 'scam']
-    train_set = {}
-    test_set = {}
-    duplicate = []
+def get_misttrack_label(coin: str, address: str) -> None:
+    mapping = {'BTC': 'BTC', 'ETH': 'ETH', 'TRON': 'TRX'}
+    api_key_mt = 'd8ab0f7a43cc49618af572fa6c5e1c0c'
+    url = ('https://openapi.misttrack.io/v1/address_labels?coin={}&address={}&api_key={}'
+           .format(mapping[coin], address, api_key_mt))
 
-    for data_list in data_lists:
-        dataset = readJson(f'./data/{data_list}_jace.json')
-        index = int(len(dataset.keys()) * split_ratio)
+    response = requests.get(url=url).json()
 
-        for i, key in enumerate(dataset.keys()):
-            feature = dataset[key]
-
-            feature['Label'] = data_list
-
-            if key in train_set.keys() or key in test_set.keys():
-                duplicate.append(key)
-            if i < index:
-                train_set[key] = feature
-            else:
-                test_set[key] = feature
-
-    for key in duplicate:
-        try:
-            del train_set[key]
-        except:
-            pass
-        try:
-            del test_set[key]
-        except:
-            pass
-    
-    for key in duplicate:
-        if key in train_set.keys() or key in test_set.keys():
-            print('attention')
-
-    print(len(train_set.keys()))
-    print(len(test_set.keys()))
-    print(len(duplicate))
-
-    saveJson(save_path=f'./data/train_set_{version}.json', wallet_list=train_set)
-    saveJson(save_path=f'./data/test_set_{version}.json', wallet_list=test_set)
+    return response
 #
 if __name__ == '__main__':
-    func_1(split_ratio=0.8, version=1)
+    response = get_misttrack_label(coin='BTC', address='3JEk9g87E9CEnxNWk1foxyVW27YEAmqaWQ')
+    print(response)
 #
