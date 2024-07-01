9edfc12287f7f131edd1f994ed2bd4b61c22c529
Chen-Si-An <b00611024@gmail.com>
2024-07-01 11:24:45 +0800

    query degree with threshold

--- src/dal_btc.py
+++ src/dal_btc.py
@@ -1,5 +1,6 @@
 from neo4j import GraphDatabase
 import time
+import utils
 
 class Neo4jConnection:
     
@@ -103,40 +104,65 @@ class Neo4jConnection:
         finally:
             if session is not None:
                 session.close()
-    
-    def get_accounts(self, db_name: str):
-        query = """
-        MATCH (a:account)
-        RETURN a.address AS address
-        LIMIT 1000000
-        """
-    
-        session = None
-        response = None
-        # accounts = []
-        try:
-            session = self.__driver.session(database=db_name)
-            response = list(session.run(query))
-            # for record in response:
-            #     accounts.append(record['address'])
-        except Exception as e:
-            print("Query failed:", e)
-        finally:
-            if session is not None:
-                session.close()
-        # return accounts
+        return response
+# 
+def init_db(uri, user, password):
+    global conn
+    conn = Neo4jConnection(uri, user, password)
+# 
+def get_degree(db_name, address):
+    # Execute a Cypher query specifying the database name
+    query_degree = """
+    MATCH (a:account {address: $node_address})
+    RETURN a.address AS address, size([(a)<--() | 1]) AS in_degree, size([(a)-->() | 1]) AS out_degree
+    """
+
+    parameters = {"node_address": address}
+    query_result = conn.query(query_degree, parameters=parameters, db=db_name)
+    if query_result:
+        in_degree, out_degree = query_result[0]['in_degree'], query_result[0]['out_degree']
+        print(f"Address: {address}, Indegree: {in_degree},  Outdegree: {out_degree}")
+        return in_degree, out_degree
+    else:
+        return None, None
+# 
+def get_neighbors(db_name, address, limit):
+    query_neighbors = """
+    MATCH (ac:account {address: $node_address}) -[:input|output]-(:transaction)-[:input|output]-(an:account)
+    WHERE ac <> an
+    RETURN DISTINCT an.address AS nb_address
+    LIMIT $limit
+    """
+
+    parameters = {"node_address": address, "limit": limit}
+    query_result = conn.query(query_neighbors, parameters=parameters, db=db_name)
+    nb_list = []
+    for record in query_result:
+        nb_list.append(record['nb_address'])
+    return nb_list
+#
+def get_address_by_degree(db_name, upper_bound, lower_bound):
+    query_address_by_degree = """
+    MATCH (a:account)
+    WITH a.address AS address, size([(a)<--() | 1]) AS in_degree, size([(a)-->() | 1]) AS out_degree
+    WHERE (in_degree + out_degree) <= $upper_bound AND (in_degree + out_degree) >= $lower_bound
+    RETURN address, in_degree, out_degree
+    """
+
+    parameters = {"upper_bound": upper_bound, "lower_bound": lower_bound}
+    query_result = conn.query(query_address_by_degree, parameters=parameters, db=db_name)
+    address_list = {}
+    for record in query_result:
+        address_list[record['address']] = (record['in_degree'], record['out_degree'])
+    return address_list
+# Close the connection
+def close_db():
+    conn.close()
 
 if __name__ == '__main__':
-    conn = Neo4jConnection("bolt://192.168.200.83:7687/", "reader", "P@ssw0rd")
-    # indegree, outdegree = conn.get_degree(db_name='bitcoin', address='bc1q4c8n5t00jmj8temxdgcc3t32nkg2wjwz24lywv')
-    # print(indegree, outdegree)
-    # neighbors = conn.get_neighbors(db_name='bitcoin', address='bc1q4c8n5t00jmj8temxdgcc3t32nkg2wjwz24lywv', limit=10)
-    # print(neighbors)
-    # address_list = conn.get_address_by_degree(db_name='bitcoin3', upper_bound=float('inf'), lower_bound=10000)
-    # print(address_list)
-    start_time = time.time()
-    print("Start process of getting all accounts")
-    accounts = conn.get_accounts(db_name='bitcoin5')
-    print("End process of getting all accounts")
-    end_time = time.time()
-    print(f'elapsed time: {end_time - start_time} seconds')
+    init_db("bolt://192.168.200.83:7687/", "reader", "P@ssw0rd")
+    indegree, outdegree = get_degree(db_name='bitcoin', address='bc1q4c8n5t00jmj8temxdgcc3t32nkg2wjwz24lywv')
+    print(indegree, outdegree)
+    nb_list = get_neighbors(db_name='bitcoin', address='bc1q4c8n5t00jmj8temxdgcc3t32nkg2wjwz24lywv', limit=5)
+    utils.saveJson('./nb_list.json', nb_list)
+    close_db()
